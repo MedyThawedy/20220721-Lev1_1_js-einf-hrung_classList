a181babe61f5e635419cbfaf6ca2b5f79c67d224
MD <dhmedy@googlemail.com>
2022-07-21 14:07:51 +0200

    c

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,9 @@ function fn_new_entries() {
     olTodoList.innerHTML = olTodoList.innerHTML + `<li class="listItem">${todos}</li>`;
 
 }
+
+
+function fn_add_style() {
+    const list = document.getElementById("olTodoList").classList;
+    list.add("clsOlTodoList");
+}
